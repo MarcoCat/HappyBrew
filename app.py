c11389cc9b5d86ec258e11e05b662cb7b482a8ad
Marco <mpoon25@my.bcit.ca>
2023-05-01 23:00:03 -0700

    adds routing between pages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
@@ -15,6 +15,36 @@ def index():
     with open("menu.json") as f:
         menu = json.load(f)
     return render_template("menu.html", menu=menu)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/instructions")
+def contact():
+    return render_template("instructions.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("loginsignup.html")
+
+
+@app.route("/customize")
+def customize():
+    return render_template("customize1.html")
+
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
 
 
 if __name__ == "__main__":
