26863961d8eeb9c4e6b09853e11ab9c5a64e392a
Marco <mpoon25@my.bcit.ca>
2023-05-01 17:12:29 -0700

    adds routing to home page

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def index():
     return render_template("index.html", menu=menu)
 
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
     app.run()
