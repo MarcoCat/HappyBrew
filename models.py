7f601d8e2c1a4bc898950b8d803a8a0c198a7062
Marco Poon <marcopoon217@gmail.com>
2023-05-04 15:12:54 -0700

    creates Order and ProductsOrder tables

--- models.py
+++ models.py
@@ -16,3 +16,49 @@ class Product(db.Model):
             "description": self.description,
             "quantity": self.quantity,
         }
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    products = db.relationship("ProductsOrder", back_populates="order")
+
+    def to_dict(self):
+        return {
+            "customer_name": self.name,
+            "customer_address": self.address,
+            "products": [
+                {
+                    "name": product.name,
+                    "quantity": product.quantity,
+                }
+                for product in self.products
+            ],
+            "price": round(
+                sum(product.price * product.quantity for product in self.products),
+                2,
+            ),
+        }
+
+    def process(self):
+        if self.completed:
+            return
+
+        for product_order in self.products:
+            product = product_order.product
+            if product.quantity < product_order.quantity:
+                product_order.quantity = product.quantity
+            product.quantity -= product_order.quantity
+
+        self.completed = True
+        db.session.commit()
+
+
+class ProductsOrder(db.Model):
+    product_name = db.Column(db.ForeignKey("product.name"), primary_key=True)
+    order_id = db.Column(db.ForeignKey("order.id"), primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    product = db.relationship("Product")
+    order = db.relationship("Order", back_populates="products")
