c9caed0149639f744acb241497c9d20028809d94
Marco Poon <marcopoon217@gmail.com>
2023-05-04 14:52:55 -0700

    adds quantity to product

--- create_products.py
+++ create_products.py
@@ -2,54 +2,44 @@ from app import app, db
 from models import Product
 
 products = [
-    {
-        "name": "Mango Smoothie",
-        "price": 6.50,
-        "category": "Seasonal Only",
-        "description": "Special seasonal fruit",
-    },
-    {
-        "name": "Grape Smoothie",
-        "price": 6.50,
-        "category": "Seasonal Only",
-        "description": "Special seasonal fruit with bubble",
-    },
-    {
-        "name": "Watermelon and Coconut Slush",
-        "price": 7.50,
-        "category": "Seasonal Only",
-        "description": "Special seasonal fruit with Sago",
-    },
-    {
-        "name": "Milk Tea",
-        "price": 4.50,
-        "category": "Milk Tea Series",
-        "description": "Just a normal milk tea",
-    },
-    {
-        "name": "Bubble Tea",
-        "price": 5.00,
-        "category": "Milk Tea Series",
-        "description": "Milk tea but it has pearls this time",
-    },
-    {
-        "name": "Black Tea Latte",
-        "price": 4.50,
-        "category": "Milk Tea Series",
-        "description": "A delicious milk tea with more black tea base",
-    },
-    {
-        "name": "Bubble Tea Latte",
-        "price": 4.50,
-        "category": "Milk Tea Series",
-        "description": "A creamy milk tea with chewy pearls",
-    },
-    {
-        "name": "Green Tea",
-        "price": 4.00,
-        "category": "Tea",
-        "description": "Just a normal green tea",
-    },
+    ("Mango smoothie", 6.50, "Seasonal only", "Special seasonal fruit", 100),
+    (
+        "Grape smoothie",
+        6.50,
+        "Seasonal only",
+        "Special seasonal fruit with bubble",
+        100,
+    ),
+    (
+        "Watermelon and Coconut Slush",
+        7.50,
+        "Seasonal only",
+        "Special seasonal fruit with Sago",
+        100,
+    ),
+    ("Milk Tea", 4.50, "Milk Tea Series", "Just a normal milk tea", 100),
+    (
+        "Bubble Tea",
+        5.00,
+        "Milk Tea Series",
+        "Milk tea but it has pearls this time",
+        100,
+    ),
+    (
+        "Black Tea Latte",
+        4.50,
+        "Milk Tea Series",
+        "A delicious milk tea with more black tea base",
+        100,
+    ),
+    (
+        "Bubble Tea Latte",
+        4.50,
+        "Milk Tea Series",
+        "A creamy milk tea with chewy pearls",
+        100,
+    ),
+    ("Green Tea", 4.00, "Tea", "Just a normal green tea", 100),
 ]
 
 
--- models.py
+++ models.py
@@ -5,10 +5,13 @@ class Product(db.Model):
     name = db.Column(db.String, unique=True, primary_key=True, nullable=False)
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
             "name": self.name,
             "price": self.price,
             "category": self.category,
+            "description": self.description,
         }
