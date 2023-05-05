356e157a47dbfff715942d1bfb32de2835c3fd39
Dannykhvn <daniel.khvn@gmail.com>
2023-05-04 22:37:53 -0700

    Created event listener for decrease button to remove one when clicked

--- menu.js
+++ menu.js
@@ -11,3 +11,15 @@
     });
   });
 
+  // Get all the buttons with class "removeButton"
+  const removeButtons = document.querySelectorAll('.removeButton');
+  
+  // Add event listener to each button
+  removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Get the count input element for the clicked button
+      const countInput = button.nextElementSibling;
+      // Decrease the value of the input element by 1, but don't go below 1
+      countInput.value = parseInt(countInput.value) - 1 < 1 ? 1 : parseInt(countInput.value) - 1;
+    });
+  });
