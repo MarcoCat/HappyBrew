7ea8ef966aa0ba33503526e9bfb556ae7bb434a4
Dannykhvn <daniel.khvn@gmail.com>
2023-05-04 23:07:04 -0700

    created new button to add to cart

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,3 +1,30 @@
+  // Add event listener to all "Add to cart" buttons
+const addToCartButtons = document.querySelectorAll('.addToCartButton');
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the product container element for the clicked button
+    const productContainer = button.closest('.itemType li');
+
+    // Extract the product name, price, and quantity from the HTML
+    const productName = productContainer.querySelector('h3').textContent;
+    const productPrice = parseFloat(productContainer.querySelector('p.price').textContent.split(':')[1].trim());
+    const productQuantity = parseInt(productContainer.querySelector('input.count').value);
+
+    // Create a JavaScript object with the product data
+    const product = {
+      name: productName,
+      price: productPrice,
+      quantity: productQuantity
+    };
+
+    // Add the product to the cart in localStorage
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    cartItems.push(product);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  });
+});
+  
   // Get all the buttons with class "addButton"
   const addButtons = document.querySelectorAll('.addButton');
   
