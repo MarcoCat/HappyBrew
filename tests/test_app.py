db360cfb12a14ff263b5ea3a7fd38b1199913eb8
Marco <marcopoon217@gmail.com>
2023-05-11 01:47:10 -0700

    adds test to check successful signup

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,8 +8,33 @@ def test_routing(test_client):
         assert response.status_code == 200
 
 
-def test_signup(test_client, new_user):
-    # test that user can sign up
+def test_signup_successful(test_client):
+    new_user = {"username": "test1", "password": "test1234"}
     response = test_client.post("/signup", data=new_user)
-    assert response.status_code == 302  # redirect to login page
+    assert response.status_code == 302
     assert User.query.filter_by(username=new_user["username"]).first() is not None
+
+
+def test_signup_password_mismatch(test_client):
+    new_user = {
+        "username": "test2",
+        "password": "test1234",
+        "confirm_password": "test12345",
+    }
+    response = test_client.post("/signup", data=new_user)
+    assert response.status_code == 200
+    assert "Passwords do not match" in response.data.decode("utf-8")
+
+
+def test_signup_username_taken(test_client):
+    new_user = {
+        "username": "test3",
+        "password": "test1234",
+        "confirm_password": "test1234",
+    }
+    response = test_client.post("/signup", data=new_user)
+    assert response.status_code == 302
+    assert User.query.filter_by(username=new_user["username"]).first() is not None
+    response = test_client.post("/signup", data=new_user)
+    assert response.status_code == 200
+    assert "Username already taken" in response.data.decode("utf-8")
