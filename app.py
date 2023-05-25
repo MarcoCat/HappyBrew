7a5e1dbd76f4f507bb1439b0cce552c57d35147b
Katy Chan <arrru0903@gmail.com>
2023-05-25 16:55:28 -0700

    onlu display complete order in dashboard

--- app.py
+++ app.py
@@ -165,11 +165,12 @@ def signup():
 
     return render_template("signup.html")
 
+    
 
 @app.route("/dashboard")
 def dashboard():
     if current_user.is_authenticated:
-        orders = Order.query.filter_by(name=current_user.username).all()
+        orders = Order.query.filter_by(name=current_user.username, completed=True).all()
         return render_template("dashboard.html", user=current_user, orders=orders)
     else:
         return redirect(url_for("login"))
